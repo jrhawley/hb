333e8fbf983af106a98dbc0c8291c48a11dbe661
James Hawley <developer@jrhawley.ca>
2022-04-10 13:14:46 -0400

    Adding some tests for date conversion
    There are some off-by-1 or -2 errors with the date conversion.
    I think this has to do with the switch between Julian and Gregorian calendars and/or leap days, but I'm not sure.
    The date that I see in HomeBank is not the same as the value I parse.
    These failing tests help highlight that.

--- src/homebank-db/src/transaction/transaction_date.rs
+++ src/homebank-db/src/transaction/transaction_date.rs
@@ -7,3 +7,29 @@ pub(crate) fn julian_date_from_u32(d: u32) -> NaiveDate {
     let julian_zero = NaiveDate::from_ymd(1, 1, 1);
     julian_zero + Duration::days(d.into())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = 4;
+        assert_eq!(2 + 2, result);
+    }
+
+    #[track_caller]
+    fn check_date_conversion(input: u32, expected: NaiveDate) {
+        let observed = julian_date_from_u32(input);
+
+        assert_eq!(expected, observed);
+    }
+
+    #[test]
+    fn unix_epoch_beginning() {
+        let input = 719163;
+        let expected = NaiveDate::from_ymd(1970, 01, 01);
+
+        check_date_conversion(input, expected);
+    }
+}
