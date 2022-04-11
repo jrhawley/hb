b102ed85d101472f3d7aac647438bdc9b8276ec3
James Hawley <developer@jrhawley.ca>
2022-04-11 00:25:43 -0400

    Adding unclamped date operations
    Will rarely use these, but want them there just as a special case.
    This includes tests for the function, too.

--- src/homebank-db/src/transaction/transaction_date.rs
+++ src/homebank-db/src/transaction/transaction_date.rs
@@ -19,6 +19,13 @@ pub(crate) fn julian_date_from_u32(d: u32) -> NaiveDate {
     clamp_date(julian_zero + Duration::days(d.into()))
 }
 
+pub(crate) fn unclamped_julian_date_from_u32(d: u32) -> NaiveDate {
+    // dates are stored as Julian dates, starting from 0001-01-01
+    // i.e. 0001-01-01 is day 1, so we start from the previous day to avoid off-by-1 errors
+    let julian_zero = NaiveDate::from_ymd(0000, 12, 31);
+    julian_zero + Duration::days(d.into())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -105,5 +112,52 @@ mod tests {
         let expected = NaiveDate::from_ymd(2200, 12, 31);
 
         check_clamp_date(input, expected);
+    }
+
+    #[track_caller]
+    fn check_unclamped_date(input: u32, expected: NaiveDate) {
+        let observed = unclamped_julian_date_from_u32(input);
+
+        assert_eq!(expected, observed);
+    }
+
+    #[test]
+    fn convert_date_prior_to_min_unclamped() {
+        let input = 693500;
+        let expected = NaiveDate::from_ymd(1899, 09, 27);
+
+        check_unclamped_date(input, expected);
+    }
+
+    #[test]
+    fn convert_date_equal_to_min_unclamped() {
+        let input = 693596;
+        let expected = NaiveDate::from_ymd(1900, 01, 01);
+
+        check_unclamped_date(input, expected);
+    }
+
+    #[test]
+    fn convert_date_between_bounds_unclamped() {
+        let input = 693597;
+        let expected = NaiveDate::from_ymd(1900, 01, 02);
+
+        check_unclamped_date(input, expected);
+    }
+
+    #[test]
+    fn convert_date_equal_to_upper_unclamped() {
+        let input = 803533;
+        let expected = NaiveDate::from_ymd(2200, 12, 31);
+
+        check_unclamped_date(input, expected);
+    }
+
+    #[test]
+    fn convert_date_grater_than_upper_unclamped() {
+        let input = 803534;
+        let expected = NaiveDate::from_ymd(2201, 01, 01);
+
+        check_unclamped_date(input, expected);
     }
 }
