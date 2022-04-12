454eaf48439c76b485ad8a815fec9c2a8c085786
James Hawley <developer@jrhawley.ca>
2022-04-12 09:30:25 -0400

    Adding extra methods for `SimpleTransaction`s

--- src/homebank-db/src/transaction/transaction_simple.rs
+++ src/homebank-db/src/transaction/transaction_simple.rs
@@ -1,6 +1,6 @@
 //! A simple `Transaction` that only belongs to a single `Category`.
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq)]
 pub struct SimpleTransaction<'a> {
     /// Which category does this transaction fall under
     category: Option<usize>,
@@ -15,14 +15,32 @@ pub struct SimpleTransaction<'a> {
 }
 
 impl<'a> SimpleTransaction<'a> {
-    /// Create and empty `SimpleTransaction`
+    /// Create an empty `SimpleTransaction`
     pub fn empty() -> Self {
-        Self { category: None }
+        Self {
+            category: None,
+            amount: &0.0,
+            memo: &None,
+        }
+    }
+
+    /// Create an new `SimpleTransaction`
+    pub fn new(category: Option<usize>, amount: &'a f32, memo: &'a Option<String>) -> Self {
+        Self {
+            category,
+            amount,
+            memo,
+        }
     }
 
     /// Retrieve the `Category` for the `Transaction`
     pub fn category(&self) -> &Option<usize> {
         &self.category
+    }
+
+    /// Retrieve the mutable `Category` for the `Transaction`
+    pub fn mut_category(&mut self) -> &mut Option<usize> {
+        &mut self.category
     }
 
     /// Retrieve the amount of the `Transaction`
