952a8b086670a2a8878952bf25f6d4c81dcf1cf0
James Hawley <developer@jrhawley.ca>
2022-11-04 14:54:02 -0400

    Adding comments for `HomeBankDbProperties`
    Doc comments, `auto_weekday` and `auto_nbdays`.
    Initial work on properly implementing the `ScheduleMode`.

--- src/homebank-db/src/db/db_properties.rs
+++ src/homebank-db/src/db/db_properties.rs
@@ -7,11 +7,21 @@ use xml::attribute::OwnedAttribute;
 /// Properties for the entire HomeBank database.
 #[derive(Debug, PartialEq)]
 pub struct HomeBankDbProperties {
+    /// Title for the database.
     title: String,
-    currency_idx: usize,
-    car_category_idx: usize,
-    auto_smode: usize,
-    auto_weekday: usize,
+
+    /// Key specifying the base [`Currency`][crate::currency::currency::Currency] that all conversion rates are calculated against.
+    currency_key: usize,
+
+    /// Key specifying the [`Category`][crate::category::category::Category] that contains [`Transaction`s][crate::transaction::transaction::Transaction] about your vehicle's (or vehicles') mileage and fuel consumption.
+    car_category_key: usize,
+
+    auto_smode: u8,
+    auto_weekday: u8,
+    auto_nbdays: u8,
+
+    // Mode for how automatically scheduled transactions should be added.
+    // sched_mode: ScheduleMode,
 }
 
 impl HomeBankDbProperties {
@@ -19,10 +29,12 @@ impl HomeBankDbProperties {
     pub fn empty() -> Self {
         Self {
             title: String::from(""),
-            currency_idx: 1,
-            car_category_idx: 1,
+            currency_key: 1,
+            car_category_key: 1,
+            // sched_mode: ScheduleMode::NotCurrentlySet,
             auto_smode: 1,
             auto_weekday: 1,
+            auto_nbdays: 0,
         }
     }
 
@@ -31,15 +43,17 @@ impl HomeBankDbProperties {
         title: &str,
         currency: usize,
         car_category: usize,
-        auto_smode: usize,
-        auto_weekday: usize,
+        auto_smode: u8,
+        auto_weekday: u8,
+        auto_nbdays: u8,
     ) -> Self {
         Self {
             title: title.to_string(),
-            currency_idx: currency,
-            car_category_idx: car_category,
+            currency_key: currency,
+            car_category_key: car_category,
             auto_smode,
             auto_weekday,
+            auto_nbdays,
         }
     }
 }
@@ -50,7 +64,7 @@ impl Default for HomeBankDbProperties {
     }
 }
 
-#[derive(Debug, Error)]
+#[derive(Debug, Error, PartialEq, Eq)]
 pub enum HomeBankDbPropertiesError {
     #[error("Invalid database title.")]
     InvalidTitle,
@@ -76,25 +90,31 @@ impl TryFrom<Vec<OwnedAttribute>> for HomeBankDbProperties {
                     props.title = i.value.to_string();
                 }
                 "curr" => {
-                    props.currency_idx = match usize::from_str(&i.value) {
+                    props.currency_key = match usize::from_str(&i.value) {
                         Ok(idx) => idx,
                         Err(_) => return Err(HomeBankDbPropertiesError::InvalidCurrency),
                     }
                 }
                 "car_category" => {
-                    props.car_category_idx = match usize::from_str(&i.value) {
+                    props.car_category_key = match usize::from_str(&i.value) {
                         Ok(idx) => idx,
                         Err(_) => return Err(HomeBankDbPropertiesError::InvalidVehicleCategory),
                     }
                 }
                 "auto_smode" => {
-                    props.auto_smode = match usize::from_str(&i.value) {
+                    props.auto_smode = match u8::from_str(&i.value) {
                         Ok(idx) => idx,
                         Err(_) => return Err(HomeBankDbPropertiesError::InvalidSMode),
                     }
                 }
                 "auto_weekday" => {
-                    props.auto_weekday = match usize::from_str(&i.value) {
+                    props.auto_weekday = match u8::from_str(&i.value) {
+                        Ok(idx) => idx,
+                        Err(_) => return Err(HomeBankDbPropertiesError::InvalidStartingWeekday),
+                    }
+                }
+                "auto_nbdays" => {
+                    props.auto_weekday = match u8::from_str(&i.value) {
                         Ok(idx) => idx,
                         Err(_) => return Err(HomeBankDbPropertiesError::InvalidStartingWeekday),
                     }
@@ -103,5 +123,90 @@ impl TryFrom<Vec<OwnedAttribute>> for HomeBankDbProperties {
             }
         }
         Ok(props)
+    }
+}
+
+/// Default setting for how scheduled [`Transaction`][crate::transaction::transaction::Transaction] dates should be calculated.
+pub enum ScheduleMode {
+    /// Not currently set.
+    /// This is used when creating a new HomeBank database, or when parsing the XML file for the first time.
+    NotCurrentlySet,
+
+    /// Add this many days in advance of the current date.
+    Add(u8),
+
+    /// Add days until you reach this day of each month (excluded).
+    AddUntil(u8)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use xml::{reader::XmlEvent, EventReader};
+
+    #[test]
+    fn it_works() {
+        let result = 4;
+        assert_eq!(2 + 2, result);
+    }
+
+    #[track_caller]
+    fn check_try_from_single_str(input: &str, expected: Result<HomeBankDbProperties, HomeBankDbPropertiesError>) {
+        // set up the reader from the input string
+        let mut reader = EventReader::from_str(input);
+
+        // skip the XML starting header and parse the first event
+        let (_start, first) = (reader.next(), reader.next());
+
+        // get the first event
+        if let Ok(XmlEvent::StartElement {
+            name, attributes, ..
+        }) = first
+        {
+            if "properties" == name.local_name.as_str() {
+                let observed = HomeBankDbProperties::try_from(attributes);
+                assert_eq!(expected, observed);
+            } else {
+                panic!(
+                    "Incorrect transaction string passed into check. Expected `ope`, found `{:#?}`",
+                    name.local_name.as_str()
+                );
+            }
+        } else {
+            panic!("Incorrect string passed into check. `{:#?}`", first);
+        }
+    }
+
+    #[test]
+    fn check_title() {
+        let input = r#"<properties title="DbTitle">"#;
+        let expected = Ok(HomeBankDbProperties {
+            title: "DbTitle".to_string(),
+            ..Default::default()
+        });
+
+        check_try_from_single_str(input, expected);
+    }
+    
+    #[test]
+    fn check_currency() {
+        let input = r#"<properties curr="2">"#;
+        let expected = Ok(HomeBankDbProperties {
+            currency_key: 2,
+            ..Default::default()
+        });
+
+        check_try_from_single_str(input, expected);
+    }
+    
+    #[test]
+    fn check_car_cat() {
+        let input = r#"<properties car_category="3">"#;
+        let expected = Ok(HomeBankDbProperties {
+            car_category_key: 3,
+            ..Default::default()
+        });
+
+        check_try_from_single_str(input, expected);
     }
 }
